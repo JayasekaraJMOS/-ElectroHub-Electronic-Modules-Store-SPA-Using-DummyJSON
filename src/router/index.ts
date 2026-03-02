643eadcaa5dfdab8b356fe3253ec528fc83a6e80
JayasekaraJMOS <omindusanvida1234.com>
2026-03-02 13:58:45 +0530

    feat: add registration view and route

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,11 +3,12 @@ import HomeView from '../views/HomeView.vue'
 import ProductDetailView from '../views/ProductDetailView.vue'
 import CartView from '../views/CartView.vue'
 import LoginView from '../views/LoginView.vue'
-
+import RegisterView from '../views/RegisterView.vue'
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/login', component: LoginView, meta: { title: 'Login - ElectroHub' } },
+    { path: '/register', component: RegisterView, meta: { title: 'Register - ElectroHub' } },
     { path: '/', component: HomeView, meta: { title: 'Home - ElectroHub' } },
     { path: '/product/:id', component: ProductDetailView, meta: { title: 'Product Details - ElectroHub' } },
     { path: '/cart', component: CartView, meta: { title: 'Shopping Cart - ElectroHub' } }
